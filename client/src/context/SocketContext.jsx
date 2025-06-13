5d069c6bdab43d2d64c545246398f8de6f8322bc
Aravshreyas <advaysaim18@gmail.com>
2025-06-13 17:36:09 +0530

    socket test

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,8 +15,8 @@ export const SocketProvider = ({ children }) => {
     useEffect(() => {
         if (user?._id) {
             const token = sessionStorage.getItem("accessToken");
-            const connectionOptions = isAppleDevice() && token ? { auth: { token },transports: ['websocket'] } : { withCredentials: true ,transports: ['websocket']};
-            const newSocket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:8000", connectionOptions);
+            const connectionOptions = isAppleDevice() && token ? { auth: { token } } : { withCredentials: true };
+            const newSocket = io(import.meta.env.VITE_BACKEND_URL_BASE || "http://localhost:8000", connectionOptions);
             
             newSocket.on('getOnlineUsers', (users) => setOnlineUsers(users || []));
             setSocket(newSocket);
